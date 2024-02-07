7997e118b43d558c98e9c29d24f972e1d9072223
Leeyona Ismam <leeyonaismam@gmail.com>
2024-02-08 01:27:03 +0600

    First UI

---
+++ SmartMovieTheaterManagement/SmartMovieTheaterManagement/Admin_Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMARTMOVIETHEATERMANAGEMENT
+{
+    public partial class Admin_Login : Form
+    {
+        public Admin_Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
