1e46e9d219c36274d651e62c085e17d06d486874
arpita paul <arpip99@gmail.com>
2024-02-11 02:10:06 +0600

    User Registration

---
+++ SmartMovieTheaterManagement/SmartMovieTheaterManagement/Add_New_User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMARTMOVIETHEATERMANAGEMENT
+{
+    public partial class Add_New_User : Form
+    {
+        public Add_New_User()
+        {
+            InitializeComponent();
+        }
+
+        private void Sign_Up_001_Click(object sender, EventArgs e)
+        {
+            string contactNumber = txtContactNumber.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (!string.IsNullOrEmpty(contactNumber) && !string.IsNullOrEmpty(password))
+            {
+                SaveContact(contactNumber, password);
+                MessageBox.Show("Contact saved successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please enter both contact number and password.");
+            }
+        }
+
+        private void SaveContact(string contactNumber, string password)
+        {
+            string folderPath = @"C:\SMTM\Users"; // Change the folder path as needed
+            Directory.CreateDirectory(folderPath);
+
+            string filePath = Path.Combine(folderPath, contactNumber + ".txt");
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(contactNumber);
+                writer.WriteLine(password);
+            }
+        }
+    }
+}
---
+++ SmartMovieTheaterManagement/SmartMovieTheaterManagement/Add_New_User.designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SMARTMOVIETHEATERMANAGEMENT
+{
+    partial class Add_New_User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_New_User));
+            this.Redirect_Text = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.Sign_Up_001 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.Login_Back = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.txtPassword = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtContactNumber = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Redirect_Text
+            // 
+            this.Redirect_Text.BorderColorFocused = System.Drawing.Color.White;
+            this.Redirect_Text.BorderColorIdle = System.Drawing.Color.White;
+            this.Redirect_Text.BorderColorMouseHover = System.Drawing.Color.White;
+            this.Redirect_Text.BorderThickness = 3;
+            this.Redirect_Text.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Redirect_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.Redirect_Text.ForeColor = System.Drawing.Color.White;
+            this.Redirect_Text.isPassword = false;
+            this.Redirect_Text.Location = new System.Drawing.Point(402, 464);
+            this.Redirect_Text.Margin = new System.Windows.Forms.Padding(4);
+            this.Redirect_Text.Name = "Redirect_Text";
+            this.Redirect_Text.Size = new System.Drawing.Size(472, 54);
+            this.Redirect_Text.TabIndex = 46;
+            this.Redirect_Text.Text = "Already A User? Then Click The Login Button";
+            this.Redirect_Text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Redirect_Text.Visible = false;
+            // 
+            // Sign_Up_001
+            // 
+            this.Sign_Up_001.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Sign_Up_001.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Sign_Up_001.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Sign_Up_001.BorderRadius = 0;
+            this.Sign_Up_001.ButtonText = "Sign Up";
+            this.Sign_Up_001.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Sign_Up_001.DisabledColor = System.Drawing.Color.Gray;
+            this.Sign_Up_001.Iconcolor = System.Drawing.Color.Transparent;
+            this.Sign_Up_001.Iconimage = null;
+            this.Sign_Up_001.Iconimage_right = null;
+            this.Sign_Up_001.Iconimage_right_Selected = null;
+            this.Sign_Up_001.Iconimage_Selected = null;
+            this.Sign_Up_001.IconMarginLeft = 0;
+            this.Sign_Up_001.IconMarginRight = 0;
+            this.Sign_Up_001.IconRightVisible = true;
+            this.Sign_Up_001.IconRightZoom = 0D;
+            this.Sign_Up_001.IconVisible = true;
+            this.Sign_Up_001.IconZoom = 90D;
+            this.Sign_Up_001.IsTab = false;
+            this.Sign_Up_001.Location = new System.Drawing.Point(526, 379);
+            this.Sign_Up_001.Name = "Sign_Up_001";
+            this.Sign_Up_001.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Sign_Up_001.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.Sign_Up_001.OnHoverTextColor = System.Drawing.Color.LightSalmon;
+            this.Sign_Up_001.selected = false;
+            this.Sign_Up_001.Size = new System.Drawing.Size(229, 48);
+            this.Sign_Up_001.TabIndex = 38;
+            this.Sign_Up_001.Text = "Sign Up";
+            this.Sign_Up_001.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Sign_Up_001.Textcolor = System.Drawing.Color.White;
+            this.Sign_Up_001.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sign_Up_001.Click += new System.EventHandler(this.Sign_Up_001_Click);
+            // 
+            // Login_Back
+            // 
+            this.Login_Back.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Login_Back.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Login_Back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Login_Back.BorderRadius = 0;
+            this.Login_Back.ButtonText = "Login";
+            this.Login_Back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Login_Back.DisabledColor = System.Drawing.Color.Gray;
+            this.Login_Back.Iconcolor = System.Drawing.Color.Transparent;
+            this.Login_Back.Iconimage = null;
+            this.Login_Back.Iconimage_right = null;
+            this.Login_Back.Iconimage_right_Selected = null;
+            this.Login_Back.Iconimage_Selected = null;
+            this.Login_Back.IconMarginLeft = 0;
+            this.Login_Back.IconMarginRight = 0;
+            this.Login_Back.IconRightVisible = true;
+            this.Login_Back.IconRightZoom = 0D;
+            this.Login_Back.IconVisible = true;
+            this.Login_Back.IconZoom = 90D;
+            this.Login_Back.IsTab = false;
+            this.Login_Back.Location = new System.Drawing.Point(526, 562);
+            this.Login_Back.Name = "Login_Back";
+            this.Login_Back.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Login_Back.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.Login_Back.OnHoverTextColor = System.Drawing.Color.LightSalmon;
+            this.Login_Back.selected = false;
+            this.Login_Back.Size = new System.Drawing.Size(229, 48);
+            this.Login_Back.TabIndex = 39;
+            this.Login_Back.Text = "Login";
+            this.Login_Back.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Login_Back.Textcolor = System.Drawing.Color.White;
+            this.Login_Back.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login_Back.Visible = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderColorFocused = System.Drawing.Color.White;
+            this.txtPassword.BorderColorIdle = System.Drawing.Color.White;
+            this.txtPassword.BorderColorMouseHover = System.Drawing.Color.White;
+            this.txtPassword.BorderThickness = 3;
+            this.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.txtPassword.ForeColor = System.Drawing.Color.White;
+            this.txtPassword.isPassword = false;
+            this.txtPassword.Location = new System.Drawing.Point(642, 286);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(232, 54);
+            this.txtPassword.TabIndex = 44;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(526, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 41;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtContactNumber
+            // 
+            this.txtContactNumber.BorderColorFocused = System.Drawing.Color.White;
+            this.txtContactNumber.BorderColorIdle = System.Drawing.Color.White;
+            this.txtContactNumber.BorderColorMouseHover = System.Drawing.Color.White;
+            this.txtContactNumber.BorderThickness = 3;
+            this.txtContactNumber.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtContactNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.txtContactNumber.ForeColor = System.Drawing.Color.White;
+            this.txtContactNumber.isPassword = false;
+            this.txtContactNumber.Location = new System.Drawing.Point(402, 286);
+            this.txtContactNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContactNumber.Name = "txtContactNumber";
+            this.txtContactNumber.Size = new System.Drawing.Size(232, 54);
+            this.txtContactNumber.TabIndex = 48;
+            this.txtContactNumber.Text = "Contact";
+            this.txtContactNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // Add_New_User
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.txtContactNumber);
+            this.Controls.Add(this.Redirect_Text);
+            this.Controls.Add(this.Sign_Up_001);
+            this.Controls.Add(this.Login_Back);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Add_New_User";
+            this.Text = "Add_New_User";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Bunifu.Framework.UI.BunifuMetroTextbox Redirect_Text;
+        private Bunifu.Framework.UI.BunifuFlatButton Sign_Up_001;
+        private Bunifu.Framework.UI.BunifuFlatButton Login_Back;
+        private Bunifu.Framework.UI.BunifuMetroTextbox txtPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Bunifu.Framework.UI.BunifuMetroTextbox txtContactNumber;
+    }
+}
