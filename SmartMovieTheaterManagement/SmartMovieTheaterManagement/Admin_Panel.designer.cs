b881829c70221065a8adfeb5f4ea7ef163f26df1
Leeyona Ismam <leeyonaismam@gmail.com>
2024-03-04 03:02:57 +0600

    ManageTheater from managetheater bug fix

--- SmartMovieTheaterManagement/SmartMovieTheaterManagement/Admin_Panel.designer.cs
+++ SmartMovieTheaterManagement/SmartMovieTheaterManagement/Admin_Panel.designer.cs
@@ -208,7 +208,7 @@
             this.ManageTheater.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.ManageTheater.Textcolor = System.Drawing.Color.White;
             this.ManageTheater.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ManageTheater.Click += new System.EventHandler(this.managetheater_Click);
+            this.ManageTheater.Click += new System.EventHandler(this.ManageTheater_Click);
             // 
             // pictureBox1
             // 
