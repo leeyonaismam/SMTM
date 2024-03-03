497c00903a63f6d41fb47a6b056ac8c79600fffc
Leeyona Ismam <leeyonaismam@gmail.com>
2024-03-04 01:46:30 +0600

    Check Sales Functionality

--- SmartMovieTheaterManagement/SmartMovieTheaterManagement/Program.cs
+++ SmartMovieTheaterManagement/SmartMovieTheaterManagement/Program.cs
@@ -17,7 +17,7 @@ namespace SmartMovieTheaterManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new User_Login());
+            Application.Run(new Admin_Login());
         }
     }
 }
