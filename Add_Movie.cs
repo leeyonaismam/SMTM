8292181a35c8f4f2fbe35c66814cddc5dadd6026
Arpita Paul <154439366+Arpip99@users.noreply.github.com>
2024-03-25 12:31:00 +0600

    Add files via upload

---
+++ Add_Movie.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SMARTMOVIETHEATERMANAGEMENT
+{
+    public partial class Add_Movie : Form
+    {
+        public Admin_Login admin_login = new Admin_Login();
+
+        string adminIDFinal = Admin_Login.adminID;
+
+        List<string> matchingFiles = new List<string>();
+        public Add_Movie()
+        {
+            InitializeComponent();
+
+            string directoryPath = @"C:\SMTM\Theater";
+            string variableToMatch = adminIDFinal;
+
+            try
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+
+                foreach (string file in files)
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        string firstLine = reader.ReadLine();
+                        if (firstLine != null && firstLine.Trim() == variableToMatch)
+                        {
+                            matchingFiles.Add(file);
+                            Choose_Theater.Items.Add(Path.GetFileName(file));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void Manage_Theater_Panel_Click(object sender, EventArgs e)
+        {
+            Admin_Panel Admin_Panel = new Admin_Panel();
+            Admin_Panel.Show();
+            this.Hide();
+        }
+
+        private void Add_Movie_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Modify the 5th line of the file content
+                string selectedFileName = matchingFiles[Choose_Theater.SelectedIndex];
+                string[] lines = File.ReadAllLines(selectedFileName);
+
+                if (lines.Length >= 5)
+                {
+                    lines[4] = Movie_Name.Text;
+
+                    // Write the modified content back to the file
+                    File.WriteAllLines(selectedFileName, lines);
+
+                    MessageBox.Show("Movie updated successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Movie Can't Be Added");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ User_Login.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMARTMOVIETHEATERMANAGEMENT
+{
+    public partial class User_Login : Form
+    {
+        public User_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Admin_Login_Button_Click(object sender, EventArgs e)
+        {
+            Admin_Login Admin = new Admin_Login();
+            Admin.Show();
+            this.Hide();
+        }
+
+        private void User_Sign_Up_Click(object sender, EventArgs e)
+        {
+            Add_New_User Add_New_User = new Add_New_User();
+            Add_New_User.Show();
+            this.Hide();
+        }
+
+        private void Login_Button_Click(object sender, EventArgs e)
+        {
+            string enteredContactNumber = txtContactNumber.Text.Trim();
+            string enteredPassword = txtPassword.Text.Trim();
+
+            ReadCredentials(enteredContactNumber, enteredPassword);
+        }
+
+        private void ReadCredentials(string username, string password)
+        {
+            string filePath = @"C:\SMTM\Users\UserRegistration.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach(string line in lines)
+                {
+                    string[] parts = line.Split(':');
+                    if (parts[0].Trim() == username && parts[1].Trim() == password) 
+                    {
+                        MessageBox.Show(username + password);
+                        MessageBox.Show("Login successful!");
+
+                        User_Panel User_Panel = new User_Panel();
+                        User_Panel.Show();
+                        this.Hide();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error reading file.");
+                return;
+            }
+        }
+    }
+}
